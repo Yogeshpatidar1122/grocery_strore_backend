f8b6e9c56ad0da17e7a687cfda01491a8e7fd665
Yogesh Patidar <yogeshpatidar340@gmail.com>
2024-08-09 12:45:52 +0530

    initial commit

---
+++ config/middlewares.js
@@ -0,0 +1,18 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'http://192.168.7.98:1337'], // Add your IP address and other allowed origins
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
